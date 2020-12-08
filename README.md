aa7ccde30133a06f3726cff80032111ad9ad4733
marius <mariusndini@yahoo.com>
2020-12-08 15:26:57 -0500

    init
    iniit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Snowflake-db-copy
 Snowflake stored procedure to copy data from one DB to another DB
+
+Stored Procedure is intended to copy data from source DB/Schema into target DB/Schema within Snowflake. Please be mindful that stored procedure will create materalized views. Logic will only copy tables currently and not views. MVs also cannot reference views. 
+
+<b>SHARE_MV_CREATE(DB_SRC STRING, SCHEMA_SRC STRING, DB_DEST STRING, SCHEMA_DEST STRING)</b>
+
+Please note the above input params:
+
+<b>DB_SRC</b> Named source database to grab tables from.
+
+<b>SCHEMA_SRC</b> Named source Schema to use to grab tables from
+
+<b>DB_DEST</b> Named destination database to land tables in
+
+<b>SCHEMA_DEST</b>Named destination schema to land tables in
+
+Please note that the s.proc will utilize <b>create SCHEMA if not exists</b> (for DB as well) as to avoid errors. Could have implications but most likely not.
+
+<b>Please also note: This codebase is not supported/maintained in any way shape or form. You are to use this at your own risk.</b>
---
+++ s.proc.sql
@@ -0,0 +1,49 @@
+create or replace procedure SHARE_MV_CREATE(DB_SRC STRING, SCHEMA_SRC STRING, DB_DEST STRING, SCHEMA_DEST STRING)
+    returns array
+    language javascript
+    strict
+    execute as owner
+    as
+$$
+
+var output = [];
+var getTables = `SELECT TABLE_NAME FROM ${DB_SRC}.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '${SCHEMA_SRC}'`;
+try {
+  var tables = snowflake.execute({sqlText: getTables})
+  while(tables.next()){
+      var TABLE_NAME = tables.getColumnValue(1);
+
+      //create DATABASE if not exists
+      snowflake.execute ({sqlText: `create DATABASE if not exists ${DB_DEST}`});
+
+      //create SCHEMA if not exists
+      snowflake.execute ({sqlText: `create SCHEMA if not exists ${DB_DEST}.${SCHEMA_DEST}`});
+
+      //Create MVs on all the tables above
+      snowflake.execute ({sqlText: ` create or replace secure materialized view ${DB_DEST}.${SCHEMA_DEST}.${TABLE_NAME} as
+                                     select * from ${DB_SRC}.${SCHEMA_SRC}.${TABLE_NAME} `});
+
+      //log table create time for output
+      output.push(`${DB_SRC}.${SCHEMA_SRC}.${TABLE_NAME} materialized : ${new Date()}`)
+  }
+
+}catch (err)  {
+    return "Failed: " + err;   // Return a success/error indicator.
+}
+
+return output;
+
+$$;
+
+// CALL STORED PROC
+call SHARE_MV_CREATE('ZEPL_US_STOCKS_DAILY', 'PUBLIC', 'TESTDB', 'MY_SCHEMA');
+
+
+
+
+
+
+
+
+
+
